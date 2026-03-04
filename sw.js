49e42b722181031221ced2cb64562555478620af
sdebu2000 <s.debu2000@gmail.com>
2026-03-04 08:59:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mytask-cache-v3";
+const CACHE_NAME = "mytask-cache-v5";
 
 const FILES_TO_CACHE = [
   "./",
@@ -38,6 +38,7 @@ self.addEventListener("fetch", event => {
   );
 
 });
+
 
 
 
